d221c5c1cae7708c74ddc375945838ff099b6483
Алексей Вотинцев <s02180620@gse.cs.msu.ru>
2021-03-09 09:57:49 +0300

    fourth version

--- 20210220_1/task1.py
+++ 20210220_1/task1.py
@@ -12,12 +12,14 @@ class Application(tk.Frame):
         self.time.set(time.strftime("%c"))
 
     def createWidgets(self):
+        self.timeButton = tk.Button(self, text='Time', command=self.showtime)
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
+        self.timeLabel = tk.Label(self, textvariable=self.time)
         self.timeButton.grid()
         self.quitButton.grid(row=0, column=1)
         self.timeLabel.grid(columnspan=2)
 
 
-app.master.title('Sample timer application')
 app = Application()
+app.master.title('Sample timer application')
 app.mainloop()
